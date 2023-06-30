1962a1217a48d58f35eb996cbfd8625962bc3f32
Oscarquell <oscarquell96@gmail.com>
2023-07-01 03:43:27 +0600

    finally

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.post('/send-feedback', (req, res) => {
     });
 });
 
-const port = process.env.PORT || 3001;
+const port = 3001;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
